a79c1f2a460a09de56faaffafa70d1c28d9b5d8e
techosaanandu <techosaanandu@gmail.com>
2025-03-28 11:21:17 +0530

    first commit

--- src/app/components/SearchFilter.js
+++ src/app/components/SearchFilter.js
@@ -1,7 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
 
 const schools = [
-  { value: "sri_sri_academy", label: "Sri Sri Acadamy,Ezhukone" },
+  // { value: "sri_sri_academy", label: "Sri Sri Acadamy,Ezhukone" },
+  { value: "Karickam_International_Public_School", label: "Karickam International Public School" },
 
 ];
 
